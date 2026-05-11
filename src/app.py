abca46b2bf468587f1c02273df75b694effba9bf
Devaraja <52833286+devarajaraju@users.noreply.github.com>
2026-05-10 21:11:17 -0600

    Implement createJIRA endpoint for issue creation
    Added a new endpoint to create JIRA issues with specified fields and authentication.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,65 @@
 from flask import Flask, jsonify
 import os
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 app = Flask(__name__)
 
+@app.route("/createJIRA".methods=['POST'])
+def createJIRA():
+    url = "https://devarajakraju.atlassian.net//rest/api/3/issue"
+
+auth = HTTPBasicAuth("devarajakraju@gmail.com", $JIRA_TOKEN)
+
+headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+}
+
+payload = json.dumps( {
+  "fields": {
+     "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "Order entry fails when selecting supplier.",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "issuetype": {
+      "id": "10000"
+    },    
+    "summary": "Main order flow broken",
+    "timetracking": {
+      "originalEstimate": "10",
+      "remainingEstimate": "5"
+    },
+    "versions": [
+      {
+        "id": "10000"
+      }
+    ]
+  },
+  "update": {}
+} )
+
+response = requests.request(
+   "POST",
+   url,
+   data=payload,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
 @app.route('/health')
 def health():
     return jsonify({
